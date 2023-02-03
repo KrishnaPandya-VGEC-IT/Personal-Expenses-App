2b28d40ae51deca6eba393bf2a08d6a832600e31
Krishna Pandya <62984510+KrishnaPandya-VGEC-IT@users.noreply.github.com>
2023-02-03 15:50:21 -0700

    Updating main.dart

--- main.dart
+++ main.dart
@@ -1,117 +1,132 @@
-import 'dart:math';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:personal_expenses_app/widgets/chart.dart';
+import 'package:sample_project/widgets/chart.dart';
+import 'package:sample_project/widgets/new_transaction.dart';
+import 'package:sample_project/widgets/transaction_list.dart';
+import 'models/transaction.dart';
+import 'package:intl/intl.dart';
 
-import './widgets/new_transaction.dart';
-import './widgets/transaction_list.dart';
-import './models/transaction.dart';
+void main() => runApp(MyApp());
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget
-{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Personal Expenses App",
-      // theme: ThemeData(
-      //   primarySwatch: Colors.purple,
-      // ),
+      title: 'Flutter App',
       home: MyHomePage(),
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+        accentColor: Colors.amber,
+        fontFamily: 'QuickSand',
+        textTheme: ThemeData.light().textTheme.copyWith(headline1: TextStyle(fontFamily: 'OpenSans',fontWeight: FontWeight.bold,fontSize: 18)),
+        appBarTheme: AppBarTheme(textTheme: ThemeData.light().textTheme.copyWith(headline1: TextStyle(fontFamily: 'OpenSans',fontSize: 20,fontWeight: FontWeight.bold)),)
+        // appBarTheme: AppBarTheme(
+        //   color: Colors.purple,
+        // ),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget
-{
+class MyHomePage extends StatefulWidget {
   // String titleInput;
   // String amountInput;
-  // final titleController = TextEditingController();
-  // final amountController = TextEditingController();
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final List<Transaction> _userTransaction = [
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+  final List<Transaction> _userTransactions = [
     // Transaction(
-    //     id: 't1', title: 'Gaming mouse', price: 19.99, date: DateTime.now()),
+    //   id: 't1',
+    //   title: 'New Shoes',
+    //   amount: 69.99,
+    //   date: DateTime.now(),
+    // ),
     // Transaction(
-    //     id: 't2', title: 'Groceries', price: 50.00, date: DateTime.now()),
+    //   id: 't2',
+    //   title: 'Weekly Groceries',
+    //   amount: 16.53,
+    //   date: DateTime.now(),
+    // ),
   ];
 
   List<Transaction>get _recentTransactions
   {
-    return _userTransaction.where((element) {
+    return _userTransactions.where((element)
+    {
       return element.date.isAfter(DateTime.now().subtract(Duration(days:7)));
     }).toList();
   }
 
-  void _addTransaction(String title, double amount, DateTime chosenDate)
-  {
-    final Transaction newTx = new Transaction(id: DateTime.now().toString(), title: title, price: amount, date: chosenDate);
+  void _addNewTransaction(String txTitle, double txAmount, DateTime chosenDate) {
+    final newTx = Transaction(
+      title: txTitle,
+      amount: txAmount,
+      date: chosenDate,
+      id: DateTime.now().toString(),
+    );
     setState(() {
-      _userTransaction.add(newTx);
+      _userTransactions.add(newTx);
+    });
+  }
+
+  void _startAddNewTransaction(BuildContext context) {
+    showModalBottomSheet(context: context, builder: (_) {
+      return SingleChildScrollView(child: NewTransaction(_addNewTransaction));
     });
   }
 
   void _deleteTransaction(String id)
   {
-      setState(() {
-        _userTransaction.removeWhere((transaction)
-        {
-          return transaction.id == id;
-        });
+    setState(() {
+      _userTransactions.removeWhere((element)
+      {
+          return element.id == id;
       });
-  }
-
-  void _startAddNewTransaction(BuildContext context)
-  {
-    showModalBottomSheet(context: context, builder: (_) {
-      return NewTransaction(_addTransaction);
     });
   }
 
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.purple,
-      accentColor: Colors.amber,
-      fontFamily: 'Quicksand',
-        appBarTheme: AppBarTheme(textTheme: ThemeData.light().textTheme.copyWith(headline1: TextStyle(fontFamily: 'OpenSans',fontSize: 20, fontWeight: FontWeight.bold)),),
-        textTheme: ThemeData.light().textTheme.copyWith(
-          headline1: TextStyle(fontFamily: 'OpenSans',fontWeight: FontWeight.bold, fontSize: 18),
-          button: TextStyle(color: Colors.white),
-          ),
-      ),
-      home: Scaffold(
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
         appBar: AppBar(
-          title: Text("Personal Expenses App"),
+          title: Text('Kishan Personal Expenses'),
           actions: [
-            IconButton(icon: Icon(Icons.add), onPressed: () => _startAddNewTransaction(context),),
+            IconButton(icon: Icon(Icons.add), onPressed: () {
+              _startAddNewTransaction(context);
+            })
           ],
         ),
         body: SingleChildScrollView(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
+            // mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Chart(_recentTransactions),
-              TransactionList(_userTransaction, _deleteTransaction),
+            children: <Widget>[
+              Container(
+                alignment: Alignment.center,
+                margin: EdgeInsets.only(top: 10,bottom: 10,left: 30,right: 30),
+                child: Text('Last 7 days expenses',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,color: Theme.of(context).primaryColor),),
+              ),
+              Container(
+                width: double.infinity,
+                child: Chart(_userTransactions),
+                // child: Card(
+                //   color: Colors.blue,
+                //   child: Text('CHART!'),
+                //   elevation: 5,
+                // ),
+              ),
+              TransactionList(_recentTransactions, _deleteTransaction),
             ],
           ),
         ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat ,
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
         floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: () => _startAddNewTransaction(context),
-        ),
-      ),
-    );
+            child: Icon(Icons.add), onPressed: () {
+          _startAddNewTransaction(context);
+        }),
+      );
+    }
   }
-}
